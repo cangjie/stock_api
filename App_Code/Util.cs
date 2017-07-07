3f77d8d21caab9ceee073cbb881a4f341d195a19
cangj <cangj@VW-WIN10-DEV>
2017-07-07 15:09:05 +0800

    15min long

--- App_Code/Util.cs
+++ App_Code/Util.cs
@@ -245,8 +245,8 @@ public class Util
     {
         DateTime dateRef = DateTime.Parse(date.ToShortDateString());
         bool ret = false;
-        if ((date >= DateTime.Parse(dateRef.ToShortDateString() + " 9:30") && date <= DateTime.Parse(dateRef.ToShortDateString() + " 11:30"))
-            || (date >= DateTime.Parse(dateRef.ToShortDateString() + " 13:00") && date <= DateTime.Parse(dateRef.ToShortDateString() + " 15:00")))
+        if ((date >= DateTime.Parse(dateRef.ToShortDateString() + " 9:30") && date <= DateTime.Parse(dateRef.ToShortDateString() + " 11:45"))
+            || (date >= DateTime.Parse(dateRef.ToShortDateString() + " 13:00") && date <= DateTime.Parse(dateRef.ToShortDateString() + " 15:15")))
             ret = true;
         return ret && IsTransacDay(dateRef);
     }
