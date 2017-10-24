4edf142601343d64fc1b3e4df252dbd254d4cd1e
cangj <cangj@VW-WIN10-DEV>
2017-10-24 15:24:34 +0800

    kdj away from 50

--- App_Code/Stock.cs
+++ App_Code/Stock.cs
@@ -229,6 +229,8 @@ public class Stock
             }
             else
             {
+                if ((kLineDay[i].j < 60 && kLineDay[i].j > 40) || (kLineDay[i].d < 60 && kLineDay[i].d > 40) || (kLineDay[i].k < 60 && kLineDay[i].k > 40))
+                    days = -1;
                 break;
             }
         }
